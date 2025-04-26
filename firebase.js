8473d66263df395a18ca587818319a8e8962437f
Ameera Attiah <ameera.attiah@gmail.com>
2025-04-26 03:01:16 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,6 +1,5 @@
 // firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.4/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/9.6.4/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.4/firebase-firestore.js";
 
 import {
